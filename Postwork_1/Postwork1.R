b693f130965478e3eaa3b08feedd0039c9908021
450fausto <74079377+450fausto@users.noreply.github.com>
2021-07-07 22:29:42 -0500

    Actualización 070721.2230

--- Postwork_1/Postwork1.R
+++ Postwork_1/Postwork1.R
@@ -31,18 +31,23 @@ GolesVisitante<-data.frame(Aa$FTAG)
 #   La probabilidad (marginal) de que el equipo que juega en 
 # casa anote x goles (x = 0, 1, 2, ...)
 
-JJG<-sum(table(GolesLocal)) # En general
-P_Local_G=data.frame(table(GolesLocal)/JJG) # En general
+JJG<-sum(table(GolesLocal)) # Juegos jugados
+P_Local_G=data.frame(table(GolesLocal)/JJG) 
 
 NM<-c('Goles', 'Probabilidad')
 names(P_Local_G)<-NM
+
+print(paste('La probabilidad marginal de que un equipo anote cierto número',
+            'de goles de local, se muetra en la tabla siguiente:'))
 P_Local_G
 
 # La probabilidad (marginal) de que el equipo que juega como 
 # visitante anote y goles (y = 0, 1, 2, ...)
 
-P_Visit_G=data.frame(table(GolesVisitante)/JJG) # En General
+P_Visit_G=data.frame(table(GolesVisitante)/JJG)
 names(P_Visit_G)<-NM
+print(paste('La probabilidad marginal de que un equipo anote cierto número',
+            'de goles de visitante, se muetra en la tabla siguiente:'))
 P_Visit_G
 
 # La probabilidad (conjunta) de que el equipo que juega en 
@@ -53,5 +58,7 @@ GolesLocalVisit<-data.frame(GolesLocal,GolesVisitante)
 names(GolesLocalVisit) <- c("G.Local", "G.Visit")
 GolesLocalVisit <- table(GolesLocalVisit)
 GolesLocalVisit <- GolesLocalVisit/JJG
-print('Hola mundo')
+print(paste('La probabilidad conjunta de que, tanto el equipo local como el',
+            'visitante anoten cierto número de goles, se muetra en la tabla',
+            ' siguiente:'))
 GolesLocalVisit
