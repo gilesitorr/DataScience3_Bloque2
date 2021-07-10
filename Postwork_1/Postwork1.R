0ce08de40ab46ab417a9a2f921d21e1e9e9ed419
450fausto <74079377+450fausto@users.noreply.github.com>
2021-07-10 03:09:48 -0500

    Actualización 100721.0309

--- Postwork_1/Postwork1.R
+++ Postwork_1/Postwork1.R
@@ -1,35 +1,32 @@
-############################################################
-#                   POSTWORK 01                            #
-############################################################
+###############################################################################
+#                               POSTWORK 01                                   #
+###############################################################################
 
-# Importa los datos de soccer de la temporada 2019/2020 de la 
-# primera división de la liga española a R, los datos los 
-# puedes encontrar en el siguiente enlace: 
-#   https://www.football-data.co.uk/spainm.php
+# Importa los datos de soccer de la temporada 2019/2020 de la primera división 
+# de la liga española a R, los datos los puedes encontrar en el siguiente 
+# enlace: https://www.football-data.co.uk/spainm.php
 
 ## Ubicación del archivo csv
 
 library(dplyr)
 Aa<-read.csv("https://www.football-data.co.uk/mmz4281/1920/SP1.csv")
 
-# Del data frame que resulta de importar los datos a R, extrae 
-# las columnas que contienen los números de goles anotados por 
-# los equipos que jugaron en casa (FTHG) y los goles anotados 
-# por los equipos que jugaron como visitante (FTAG)
+# Del data frame que resulta de importar los datos a R, extrae las columnas que 
+# contienen los números de goles anotados por los equipos que jugaron en casa 
+# (FTHG) y los goles anotados por los equipos que jugaron como visitante (FTAG)
 
 GolesLocal<-data.frame(Aa$FTHG)
 GolesVisitante<-data.frame(Aa$FTAG)
 
-# Consulta cómo funciona la función table en R al ejecutar en 
-# la consola ?table
+# Consulta cómo funciona la función table en R al ejecutar en la consola ?table
 
 ?table
 
-# Posteriormente elabora tablas de frecuencias relativas para 
-# estimar las siguientes probabilidades:
-#   
-#   La probabilidad (marginal) de que el equipo que juega en 
-# casa anote x goles (x = 0, 1, 2, ...)
+# Posteriormente elabora tablas de frecuencias relativas para estimar las 
+# siguientes probabilidades:
+
+# La probabilidad (marginal) de que el equipo que juega en casa anote x goles 
+# (x = 0, 1, 2, ...)
 
 JJG<-sum(table(GolesLocal)) # Juegos jugados
 P_Local_G=data.frame(table(GolesLocal)/JJG) 
@@ -41,8 +38,8 @@ print(paste('La probabilidad marginal de que un equipo anote cierto número',
             'de goles de local, se muetra en la tabla siguiente:'))
 P_Local_G
 
-# La probabilidad (marginal) de que el equipo que juega como 
-# visitante anote y goles (y = 0, 1, 2, ...)
+# La probabilidad (marginal) de que el equipo que juega como visitante anote y 
+# goles (y = 0, 1, 2, ...)
 
 P_Visit_G=data.frame(table(GolesVisitante)/JJG)
 names(P_Visit_G)<-NM
@@ -50,9 +47,9 @@ print(paste('La probabilidad marginal de que un equipo anote cierto número',
             'de goles de visitante, se muetra en la tabla siguiente:'))
 P_Visit_G
 
-# La probabilidad (conjunta) de que el equipo que juega en 
-# casa anote x goles y el equipo que juega como visitante 
-# anote y goles (x = 0, 1, 2, ..., y = 0, 1, 2, ...)
+# La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y 
+# el equipo que juega como visitante anote y goles (x = 0, 1, 2, ..., 
+# y = 0, 1, 2, ...)
 
 GolesLocalVisit<-data.frame(GolesLocal,GolesVisitante)
 names(GolesLocalVisit) <- c("G.Local", "G.Visit")
