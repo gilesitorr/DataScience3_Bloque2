483ce778082ed636e8d4c5183d6824a57aa0f9e2
450fausto <74079377+450fausto@users.noreply.github.com>
2021-07-10 18:29:32 -0500

    Alternativo

---
+++ Postwork_6/Postwork06.R
@@ -0,0 +1,32 @@
+################################################################################
+#                                 POSTWORK 06                                  #
+################################################################################
+
+# Importa el conjunto de datos match.data.csv a R y realiza lo siguiente:
+
+library(dplyr)
+
+df <- read.csv("match.data.csv")
+df$date <- as.Date(df$date, format="%Y-%m-%d")
+
+df<-mutate(df,Year.month=format(date,'%Y/%m')) 
+
+# Agrega una nueva columna sumagoles que contenga la suma de goles por partido.
+
+df <- mutate(df, sumagoles=home.score+away.score)
+
+# Obtén el promedio por mes de la suma de goles.
+
+Promedio<- df %>% group_by(Year.month) %>% summarise(gols.month=mean(sumagoles))
+
+# Crea la serie de tiempo del promedio por mes de la suma de goles hasta 
+# diciembre de 2019.
+
+PromedioHastaDic2019<-ts(subset(Promedio, Year.month<='2019/12', 
+                             select='gols.month'))
+
+# Grafica la serie de tiempo.
+plot(PromedioHastaDic2019, xlab = "Fechas jugadas", 
+     ylab = "Goles promedio", 
+     main = "Serie tiempo: promedio de goles por mes",
+     sub = "Serie mensual: de Agosto de 2010 hasta Diciembre de 2019")
