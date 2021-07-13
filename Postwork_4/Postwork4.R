46c58d9c4647787cee6421be5ed9236db6997bd9
450fausto <74079377+450fausto@users.noreply.github.com>
2021-07-13 03:39:16 -0500

    Actualizacion 131021.0339

--- Postwork_4/Postwork4.R
+++ Postwork_4/Postwork4.R
@@ -16,7 +16,8 @@
 library(dplyr)
 
 # Variables del postwork anterior
-ProbConjunta<-read.csv("ResultadoPostwork03.csv", header=T)
+Postwork03<-'https://raw.githubusercontent.com/gilesitorr/DataScience3_Bloque2/main/Postwork_4/ResultadoPostwork03.csv'
+ProbConjunta<-read.csv(Postwork03, header=T)
 ProbConjunta$X<-NULL
 
 ProbMargLocal<- ProbConjunta %>% group_by(G.Local) %>% summarise(
